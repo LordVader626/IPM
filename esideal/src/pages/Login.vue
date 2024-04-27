1963ea78ce640c0db8e5031f5b5adf3585db6d07
TomasValente <116099924+TomasValente@users.noreply.github.com>
2024-04-27 20:29:03 +0100

    Login Error Pop Up

--- esideal/src/pages/Login.vue
+++ esideal/src/pages/Login.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="layout">
+
         <div class="card">
             <!-- Logo Mecanico -->
             <img src="../assets/images/mechanic.png" alt="Mechanic Logo" class="mecPic"/>
@@ -10,11 +11,22 @@
             <!-- Butão Login -->
             <button @click="login" class="login-button">Login</button>
         </div>
+
+        <LoginError :modalActive="modalActive" @close="toggleModal">
+            <img src="../assets/images/error.png" alt="Error" class="errorImg"/>
+            <div class="modal-content">
+                <h1>Erro ao fazer login!</h1>
+                <p>Por favor tente novamente</p>
+            </div>
+        </LoginError>
+
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import LoginError from '../components/LoginError.vue';
+import {ref} from 'vue'
 
 export default {
     data() {
@@ -23,6 +35,20 @@ export default {
             telefone: ''
         };
     },
+    setup() {
+        const modalActive = ref(false);
+
+        const toggleModal = () => {
+            modalActive.value = !modalActive.value;
+        };
+
+        return { modalActive, toggleModal };
+    },
+
+    components: {
+        LoginError,
+    },
+
     methods: {
         async login() {
             try {
@@ -36,12 +62,14 @@ export default {
                     // Redirect to services page if authentication succeeds
                     this.$router.push('/services');
                 } else {
-                    alert("Email ou senha incorretos!");
+                    //alert("Email ou senha incorretos!");
+                    this.toggleModal();
                 }
             } catch (error) {
                 // Handle authentication errors
                 console.error('Error logging in:', error);
-                alert("Erro ao fazer login. Por favor, tente novamente mais tarde.");
+                //alert("Erro ao fazer login. Por favor, tente novamente mais tarde.");
+                this.toggleModal();
             }
         }
     }
@@ -100,5 +128,22 @@ export default {
     border: none;
     margin-top: 50px;
 }
+
+.modal-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+}
+
+.errorImg {
+    display: flex;
+    width: 50px; /* Adjust the size as needed */
+    height: auto;
+    margin-bottom: 20px; /* Add some space below the image */
+}
+
 </style>
 
